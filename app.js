f25282057a301e1a10779f391e57725d6efa82c7
Ahmedkhan90 <dx.ahmedkhan@gmail.com>
2020-12-19 17:57:32 +0500

    Promt & If Else

---
+++ app.js
@@ -0,0 +1,17 @@
+var a = +prompt("Enter Your Age" , "Your Age Must Be 18+");
+if( a == 18){
+    alert("You Are Allow To Ride");{
+
+    }
+}else{
+    alert("You Are Not Allow To Ride");
+}
+
+var b = +prompt("Enter Your Age" , "Your Age Must Be 18+");
+if( b > 18){
+    alert("You Are Allow To Ride");{
+
+    }
+}else{
+    alert("You Are Not Allow To Ride");
+}
